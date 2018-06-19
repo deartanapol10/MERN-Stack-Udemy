8f3e1a990836f5d503fb9ec2afcfa53034c9b031
deartanapol10 <deartanapol10@gmail.com>
2018-06-20 00:17:08 +0700

    Second Commit with validation and more

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://ddear10:ddear10@ds261430.mlab.com:61430/mernstack-udemy'
+    mongoURI: 'mongodb://ddear10:ddear10@ds261430.mlab.com:61430/mernstack-udemy',
+    secretOrKey: 'secret' //put whatever you want
 }
